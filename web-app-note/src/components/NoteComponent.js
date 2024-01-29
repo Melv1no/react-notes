2f0bc8e3811b7bd46adabee9b7766683c014b159
Melvin OLIVET <molivet@pm.me>
2024-01-29 11:43:52 +0100

    id url

--- web-app-note/src/components/NoteComponent.js
+++ web-app-note/src/components/NoteComponent.js
@@ -1,9 +1,12 @@
 import React from "react";
+import { Link } from 'react-router-dom';
+
 
 const NoteComponent = ({ note, onClickNote, highlightSearchTermInBody, highlightSearchTerm }) => {
   return (
     <div key={note.id} onClick={() => onClickNote(note.id)}>
       <div className="Notebox">
+        <Link to={`/note/${note.id}`}>{note.title}</Link>
         <div dangerouslySetInnerHTML={{ __html: highlightSearchTerm(note.title) }} />
         <div className="Creation">{note.date}</div>
         <div className="Id">ID: {note.id}</div>
